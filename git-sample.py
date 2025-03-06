306e35a0165a02c901cc343e045cc84abe86fb89
rohanj <rohan0812.me@gmail.com>
2025-03-06 15:28:29 +0530

    second  commit

---
+++ git-sample.py
@@ -0,0 +1,14 @@
+for i in range(10):
+    print(f"hellow from pi4 times {i}")
+print("task done")
+
+#git config --global user.email "rohan0812.me@gmail.com"
+#git config --global user.name "rohanj"
+
+# echo "# pi4-coderepo" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/RohanJoydhar/pi4-coderepo.git
+# git push -u origin main
--- main.py
+++
@@ -1,3 +0,0 @@
-for i in range(10):
-    print(f"hellow from pi4 times {i}")
-print("task done")
